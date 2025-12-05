4bbfddd0c49f8bfc1ae14da9184b950c1520b28f
sourav-devit <sourav@esceneric.com>
2025-12-05 12:10:53 +0530

    Js for button and styling added

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("hello to JS");
+    function buttonClicked() {
+      alert("Button was clicked!");
+    }
+
+    window.onload = function() {
+      const button = document.querySelector("button");
+      button.addEventListener("click", buttonClicked);
+    }
